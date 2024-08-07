434be33a669d1a3f8e51da174926483075677b5c
Pilla Dipesh <pilladipesh.pd@gmail.com>
2024-08-07 21:52:47 +0530

    colorscheme updated

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,28 +2,12 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "shin",
+      colorscheme = "serene-transparent",
     },
   },
   {
-    "myypo/borrowed.nvim",
-    lazy = false,
-    priority = 1000,
-
-    version = "^0", -- Optional: avoid upgrading to breaking versions
-
-    config = function()
-      require("borrowed").setup({
-        transparent = true,
-
-        styles = {
-          comments = "italic",
-          functions = "italic",
-          constants = "italic",
-        },
-      }) -- Optional: only has to be called to change settings
-
-      -- If you are changing the config, colorscheme command has to be called after setup()
-    end,
+    "samharju/serene.nvim",
+    -- for using as main:
+    --config = function() vim.cmd.colorscheme("serene") end,
   },
 }
