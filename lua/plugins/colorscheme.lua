9bbc90a1e7ce4528c183b04dcfb244b228b7225d
pilladipesh33 <pilladipesh.pd@gmail.com>
2024-09-03 22:19:53 +0530

    colorscheme updated

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,24 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "serene-transparent",
+      colorscheme = "darkvoid",
     },
   },
+  -- {
+  --   "samharju/serene.nvim",
+  --   -- for using as main:
+  --   --config = function() vim.cmd.colorscheme("serene") end,
+  -- },
   {
-    "samharju/serene.nvim",
-    -- for using as main:
-    --config = function() vim.cmd.colorscheme("serene") end,
+    "aliqyan-21/darkvoid.nvim",
+    -- require("darkvoid").setup({
+    --   transparent = true, -- set true for transparent
+    --   glow = true, -- set true for glow effect
+    --   show_end_of_buffer = true, -- set false for not showing end of buffer
+    -- }),
+    opts = {
+      transparent = true,
+      glow = true,
+    },
   },
 }
